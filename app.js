7e24d0d7a9318bae541c43179aeca031acec6c65
Siji <ram@azlogics.com>
2024-03-28 23:27:04 +0530

    second commit

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const port = PORT || 4001;
 const app = express();
 app.use(express.json());
 app.use(routes);
-const client = new Client({ user: 'postgres', host: 'localhost', database: 'FoodDelivery', password: 'Azlogics', port: 5432, });
+// const client = new Client({ user: 'postgres', host: 'localhost', database: 'FoodDelivery', password: 'Azlogics', port: 5432, });
 
 const start = async () => {
     try {
-        await client.connect().then(() => { console.log('Connected to PostgreSQL database!'); }).catch((err) => { console.error('Error connecting to the database:', err); });
+        // await client.connect().then(() => { console.log('Connected to PostgreSQL database!'); }).catch((err) => { console.error('Error connecting to the database:', err); });
     } catch (err) {
         console.error(err);
     } finally {
@@ -25,7 +25,7 @@ const start = async () => {
 }
 
 start();
-module.exports = {client}
+module.exports = { client }
 
 
 
